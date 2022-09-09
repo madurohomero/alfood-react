43dfced501443608b8cb520f6284c2d0bb4da26d
Homero Maduro <homeromaduro@hotmail.com>
2022-09-09 19:27:07 -0300

    form adminstrativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoResturantes';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import FormRestaurante from './paginas/Administracao/Restaurantes/FormRestaurante';
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route path="/admin/restaurantes" element={<AdministracaoRestaurantes />} />
+      <Route path="/admin/restaurantes/novo" element={<FormRestaurante />} />
     </Routes>
   );
 }
